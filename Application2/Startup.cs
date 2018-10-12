a25a5427e3a39fea233c4c0fde9b3e686c41f862
Himanshu Bisht <Himanshubisht227@gmail.com>
2018-10-12 09:06:17 -0500

    intial commmit

---
+++ Application2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Application2.Startup))]
+namespace Application2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
